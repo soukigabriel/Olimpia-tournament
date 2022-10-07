17e6942c908e3f1da2809ab242a478ccc4c38fd2
Gabriel Souki <gabrielsouki@gmail.com>
2022-10-07 17:16:47 -0400

    Se añadieron las animaciones de golpear.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,12 +15,21 @@ public class PlayerController : MonoBehaviour
     Vector3 movement;
     [SerializeField] float speed;
     Rigidbody m_rigidBody;
+    [SerializeField] float jumpForce;
+    public bool canJump;
+    public LayerMask groundMask;
+    [SerializeField] float rayLenght = 2f;
+    Vector3 raycastOffset = new Vector3(0, 0.775f, 0);
 
     [Space]
     [Header("Animations variables")]
     Animator m_animator;
     const string STATE_WALKING = "isMoving";
+    const string STATE_JUMP = "isJumping";
+    const string STATE_RIGHT_PUNCH = "rightPunch";
+    const string STATE_LEFT_PUNCH = "leftPunch";
     bool facingRight = true;
+    public bool canRightPunch, canLeftPunch;
 
 
     private void Awake()
@@ -30,9 +39,23 @@ public class PlayerController : MonoBehaviour
         SetPositions();
     }
 
+    private void Start()
+    {
+        canRightPunch = true;
+        canLeftPunch = false;
+        canJump = true;
+    }
+
     private void FixedUpdate()
     {
         Move(GetAxis());
+    }
+
+    private void Update()
+    {
+        Jump();
+        Punch();
+        //Debug.DrawLine(this.transform.position + raycastOffset, this.transform.localPosition + raycastOffset + (Vector3.down * rayLenght), Color.red);
     }
 
     void SetPositions()
@@ -54,8 +77,57 @@ public class PlayerController : MonoBehaviour
     {
         m_rigidBody.velocity = new Vector3(movement.x * speed * Time.fixedDeltaTime, m_rigidBody.velocity.y, m_rigidBody.velocity.z);
         SetAnimations();
+    }
+
+    void Punch()
+    {
+        if (Input.GetButtonDown("Fire1") && !m_animator.GetCurrentAnimatorStateInfo(0).IsName("Right Punch"))
+        {
+            m_animator.SetTrigger(STATE_RIGHT_PUNCH);
+            m_animator.ResetTrigger(STATE_LEFT_PUNCH);
+        }
+        else if (Input.GetButtonDown("Fire1") && !m_animator.GetCurrentAnimatorStateInfo(0).IsName("Left Punch"))
+        {
+            m_animator.SetTrigger(STATE_LEFT_PUNCH);
+        }
 
     }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && IsTouchingTheGround() && canJump)
+        {
+            m_rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            m_animator.SetTrigger(STATE_JUMP);
+            canJump = false;
+        }
+    }
+
+    public void EndJump()
+    {
+        canJump = true;
+    }
+
+
+    bool IsTouchingTheGround()
+    {
+        if(Physics.Raycast(this.transform.position + raycastOffset, Vector2.down, rayLenght, groundMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    Vector3 GetAxis()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , 0);
+        return movement;
+    }
+
+
 
     private void SetAnimations()
     {
@@ -90,10 +162,5 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    Vector3 GetAxis()
-    {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , 0);
-        return movement;
-    }
 
 }
